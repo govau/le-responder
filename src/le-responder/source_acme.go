fd6c921b6d65765c9574fb6f67cd14adcc0e6271
Alex Sadleir <alex.sadleir@dta.gov.au>
2020-07-14 21:23:58 +1000

    ACME v2 dns-01 challenge support

--- src/le-responder/source_acme.go
+++ src/le-responder/source_acme.go
@@ -55,12 +55,12 @@ func (acs *acmeCertSource) Init() error {
 }
 
 type acmeChallenge struct {
-	Message       string          `json:"message"`
-	Authorization string          `json:"authorization_uri"`
-	Challenge     *acme.Challenge `json:"challenge"`
+	Message   string          `json:"message"`
+	Challenge *acme.Challenge `json:"challenge"`
+	Order     *acme.Order     `json:"order"`
 }
 
-func newDNSChallenge(client *acme.Client, chal *acme.Challenge, hostname, authorization string) (*acmeChallenge, error) {
+func newDNSChallenge(client *acme.Client, o *acme.Order, chal *acme.Challenge, hostname string) (*acmeChallenge, error) {
 	val, err := client.DNS01ChallengeRecord(chal.Token)
 	if err != nil {
 		return nil, err
@@ -69,9 +69,9 @@ func newDNSChallenge(client *acme.Client, chal *acme.Challenge, hostname, author
 Name:  _acme-challenge.%s.
 Value: %s`, hostname, val)
 	return &acmeChallenge{
-		Message:       msg,
-		Authorization: authorization,
-		Challenge:     chal,
+		Message:   msg,
+		Challenge: chal,
+		Order:     o,
 	}, nil
 }
 
@@ -84,25 +84,33 @@ func (acs *acmeCertSource) ManualStartChallenge(ctx context.Context, hostname st
 	defer acs.lock.Unlock()
 
 	acs.ensureRegistered(ctx)
-	authz, err := acs.acmeClient.Authorize(ctx, hostname)
+	o, err := acs.acmeClient.AuthorizeOrder(ctx, acme.DomainIDs(hostname))
 	if err != nil {
 		return nil, err
 	}
-	if authz.Status == acme.StatusValid {
-		return nil, errors.New("already authorized, no challenge needed")
-	}
 	var chal *acme.Challenge
-	for _, c := range authz.Challenges {
-		if c.Type == "dns-01" {
-			chal = c
-			break
+	if o.Status == acme.StatusReady {
+		return nil, errors.New("already authorized, no challenge needed")
+	} else if o.Status == acme.StatusPending {
+		// Satisfy all pending authorizations.
+		for _, zurl := range o.AuthzURLs {
+			z, err := acs.acmeClient.GetAuthorization(ctx, zurl)
+			if err != nil {
+				return nil, err
+			}
+
+			for _, c := range z.Challenges {
+				if c.Type == "dns-01" {
+					chal = c
+					break
+				}
+			}
+			if chal == nil {
+				return nil, errors.New("no supported challenge type found")
+			}
 		}
 	}
-	if chal == nil {
-		return nil, errors.New("no supported challenge type found")
-	}
-
-	return newDNSChallenge(acs.acmeClient, chal, hostname, authz.URI)
+	return newDNSChallenge(acs.acmeClient, o, chal, hostname)
 }
 
 func (acs *acmeCertSource) ensureRegistered(ctx context.Context) {
@@ -134,18 +142,26 @@ func (acs *acmeCertSource) CompleteChallenge(ctx context.Context, pkey *rsa.Priv
 
 	log.Println("accepting dns challenge...")
 
-	_, err := acs.acmeClient.Accept(ctx, ac.Challenge)
+	c, err := acs.acmeClient.Accept(ctx, ac.Challenge)
 	if err != nil {
 		return nil, err
 	}
-
+	log.Println(c)
 	log.Println("waiting authorization...")
-	_, err = acs.acmeClient.WaitAuthorization(ctx, ac.Authorization)
+	_, err = acs.acmeClient.WaitAuthorization(ctx, c.URI)
 	if err != nil {
 		return nil, err
 	}
 
-	return acs.issueCert(ctx, hostname, pkey)
+	// All authorizations are satisfied.
+	// Wait for the CA to update the order status.
+	log.Println("waiting order...")
+	o, err := acs.acmeClient.WaitOrder(ctx, ac.Order.URI)
+	if err != nil {
+		return nil, err
+	}
+	log.Println(o)
+	return acs.issueCert(ctx, o, hostname, pkey)
 }
 
 func (acs *acmeCertSource) AutoFetchCert(ctx context.Context, pkey *rsa.PrivateKey, hostname string) ([][]byte, error) {
@@ -211,14 +227,15 @@ func (acs *acmeCertSource) AutoFetchCert(ctx context.Context, pkey *rsa.PrivateK
 		if err != nil {
 			return nil, err
 		}
+		log.Println(o.FinalizeURL)
 	} else {
 		return nil, fmt.Errorf("invalid new order status %q", o.Status)
 	}
-
-	return acs.issueCert(ctx, hostname, pkey)
+	log.Println(o.FinalizeURL)
+	return acs.issueCert(ctx, o, hostname, pkey)
 }
 
-func (acs *acmeCertSource) issueCert(ctx context.Context, hostname string, pkey *rsa.PrivateKey) ([][]byte, error) {
+func (acs *acmeCertSource) issueCert(ctx context.Context, o *acme.Order, hostname string, pkey *rsa.PrivateKey) ([][]byte, error) {
 	csr, err := x509.CreateCertificateRequest(rand.Reader, &x509.CertificateRequest{
 		Subject: pkix.Name{
 			CommonName: hostname,
@@ -229,7 +246,7 @@ func (acs *acmeCertSource) issueCert(ctx context.Context, hostname string, pkey 
 	}
 
 	log.Println("creating cert...")
-	der, _, err := acs.acmeClient.CreateCert(ctx, csr, 0, true)
+	der, _, err := acs.acmeClient.CreateOrderCert(ctx, o.FinalizeURL, csr, true)
 	if err != nil {
 		return nil, err
 	}
